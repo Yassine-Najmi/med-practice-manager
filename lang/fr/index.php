cd1f329a8c0fd6063f4357cf051f6d92ea76f021
Yassine Najmi <yassine.najmi01@gmail.com>
2024-03-19 21:17:52 +0000

    Added text localization and translation

---
+++ lang/fr/index.php
@@ -0,0 +1,164 @@
+<?php
+return [
+    //   Services Block Four
+    'hospital-schedule' => 'Horaire d\'hospitalisation',
+    'mon_friday' => 'Lundi - Vendredi',
+    'saturday' => "Samedi",
+    'sunday' => "Dimanche",
+
+    'get-appointment' => 'Prendre un rendez-vous',
+    'lorem-ipsum' => 'Lorem Ipsum lorsqu\'un imprimeur inconnu a pris une galerie de type et l\'a mélangée pour en faire un livre spécimen de type.',
+    'appointment' => 'Rendez-vous',
+
+    'discover-benefits' => 'Découvrir les avantages',
+    'experienced-doctors' => 'Médecins expérimentés',
+    'professional-staffs' => 'Personnels professionnels dévoués',
+    'emergency-service' => 'Service d\'urgence 24 heures sur 24',
+
+    'about-hospital' => 'À propos de notre hôpital',
+    'main-focus' => 'Des soins de santé de qualité à un coût efficace sont le principal objectif de notre hôpital.',
+    'long-term-goal' => 'Notre objectif à long terme est toujours d\'obtenir des résultats complexes pour votre santé dentaire. Mais dans le processus, nous mettons également l\'accent sur vous offrir le meilleur service client. Nous faisons toujours de notre cabinet dentaire un endroit aussi sûr que possible !',
+    'learn-more' => 'En savoir plus',
+
+
+
+    'medical-quality' => 'Qualité Médicale',
+    'medical-quality-description' => 'Nous fournissons le meilleur service pour nos clients. Maintenant, prenez-le.',
+    'modern-labs' => 'Laboratoires Modernes',
+    'modern-labs-description' => 'Nous fournissons le meilleur service pour nos clients. Maintenant, prenez-le.',
+    'dedicated-doctors' => 'Médecins Dévoués 24/7',
+    'dedicated-doctors-description' => 'Nous fournissons le meilleur service pour nos clients. Maintenant, prenez-le.',
+    'cutting-edge-technology' => 'Technologie de Pointe',
+    'cutting-edge-technology-description' => 'Nous fournissons le meilleur service pour nos clients. Maintenant, prenez-le.',
+
+
+    'get-appointment' => 'Prendre un rendez-vous',
+    'appointment-description' => 'Nous fournissons le meilleur service pour nos clients. Maintenant, prenez-le.',
+    'enter-name' => 'Entrez votre nom',
+    'enter-phone' => 'Entrez votre numéro de téléphone',
+    'appointment-date' => 'Date du rendez-vous',
+    'choose-department' => 'Choisir le service',
+    'department-one' => 'Service un',
+    'department-two' => 'Service deux',
+    'department-three' => 'Service trois',
+    'department-four' => 'Service quatre',
+    'department-five' => 'Service cinq',
+    'select-doctor' => 'Sélectionner un médecin',
+    'doctor-one' => 'Médecin un',
+    'doctor-two' => 'Médecin deux',
+    'doctor-three' => 'Médecin trois',
+    'doctor-four' => 'Médecin quatre',
+    'doctor-five' => 'Médecin cinq',
+    'request-appointment' => 'Demander un rendez-vous',
+
+
+
+    'why-choose-our-medical' => 'Pourquoi Choisir Notre Service Médical',
+    'health-care-cost' => 'Des soins de santé de qualité à un coût efficace sont la principale priorité de notre hôpital.',
+    'professional-staffs' => 'Équipe Professionnelle',
+    'professional-staffs-text' => 'Nous offrons le meilleur service à nos clients. Profitez-en maintenant.',
+    'online-appointment' => 'Rendez-vous en ligne',
+    'online-appointment-text' => 'Nous offrons le meilleur service à nos clients. Profitez-en maintenant.',
+    'emergency-cares' => 'Soins d\'urgence',
+    'emergency-cares-text' => 'Nous offrons le meilleur service à nos clients. Profitez-en maintenant.',
+    'advanced-technology' => 'Technologie Avancée',
+    'advanced-technology-text' => 'Nous offrons le meilleur service à nos clients. Profitez-en maintenant.',
+    'experienced-doctors' => 'Médecins Expérimentés',
+    'experienced-doctors-text' => 'Nous offrons le meilleur service à nos clients. Profitez-en maintenant.',
+    'emergency-service' => 'Service d\'urgence 24/7',
+    'emergency-service-text' => 'Nous offrons le meilleur service à nos clients. Profitez-en maintenant.',
+
+
+    'about-us' => 'À Propos de Nous',
+    'health-care-cost' => 'Des soins de santé de qualité à un coût efficace sont la principale priorité de notre hôpital.',
+    'our-goal' => 'Notre principal objectif à long terme est toujours d\'atteindre des résultats complexes pour votre santé dentaire. Mais dans le processus, nous maintenons également notre attention sur vous offrir le meilleur service client. Nous faisons toujours de notre cabinet dentaire un endroit aussi sûr que possible!',
+    'emergency-service' => 'Service d\'Urgence 24/7',
+    'professional-staffs' => 'Équipe Professionnelle',
+    'advanced-technology' => 'Technologie Avancée',
+    'experienced-doctors' => 'Médecins Expérimentés',
+
+
+    'sunday' => 'Dimanche',
+    'monday' => 'Lundi',
+    'tuesday' => 'Mardi',
+    'wednesday' => 'Mercredi',
+    'thursday' => 'Jeudi',
+    'friday' => 'Vendredi',
+    'saturday' => 'Samedi',
+    'consultation' => 'Consultation',
+    'emergency' => 'Urgence',
+
+
+    'success_surgeries' => 'Chirurgies réussies',
+    'regular_doctors' => 'Médecins réguliers',
+    'satisfied_patients' => 'Patients satisfaits',
+    'department' => 'Département',
+
+
+    'why-choose' => 'Pourquoi Choisir Notre Hôpital',
+    'why-choose-description' => 'Des soins de santé de meilleure qualité avec des coûts efficaces sont la principale priorité de notre hôpital.',
+    'mission' => 'Mission',
+    'vision' => 'Vision',
+    'success' => 'Succès',
+    'history' => 'Histoire',
+    'long-term-goal' => 'Notre objectif à long terme principal est toujours d\'obtenir des résultats complexes pour votre santé dentaire. Mais dans le processus, nous gardons également le focus sur vous offrir le meilleur service client.',
+    'dental-office-safe' => 'Nous rendons toujours notre cabinet dentaire aussi sûr que possible ! Nous offrons le meilleur service à nos clients. Prenez-le maintenant.',
+    'learn-more' => 'En savoir plus',
+
+
+    'get-appointment' => 'Prendre un rendez-vous',
+    'full-name' => 'Nom complet',
+    'email' => 'Email',
+    'email-placeholder' => 'Entrez votre e-mail',
+    'gender' => 'Genre',
+    'select-gender' => 'Sélectionnez le genre',
+    'male' => 'Homme',
+    'female' => 'Femme',
+    'phone-number' => 'Numéro de téléphone',
+    'phone-placeholder' => 'Numéro de téléphone',
+    'appointment-date' => 'Date du rendez-vous',
+    'date-placeholder' => 'Choisissez une date',
+    'consultant-hour' => 'Heure de consultation',
+    'time-placeholder' => 'Choisissez l\'heure',
+    'submit-request' => 'Envoyer la demande',
+
+
+    'regular-schedule-title' => "Horaire régulier des services",
+    'departments' => "Départements",
+    'monday-friday' => "Lundi - Vendredi",
+    'saturday-sunday' => "Samedi - Dimanche",
+    'cardiology' => "Cardiologie",
+    'orthopedic' => "Orthopédie",
+    'neurology' => "Neurologie",
+    'dental-care' => "Soins dentaires",
+    'hepatology' => "Hépatologie",
+    'eyes-care' => "Soins oculaires",
+
+
+    'enter-your-name' => "Entrez votre nom",
+    'enter-your-phone-number' => "Entrez votre numéro de téléphone",
+    'appointment-date' => "Date de rendez-vous",
+    'choose-department' => "Choisissez le département",
+    'department-one' => "Département un",
+    'department-two' => "Département deux",
+    'department-three' => "Département trois",
+    'department-four' => "Département quatre",
+    'department-five' => "Département cinq",
+    'select-doctor' => "Sélectionnez le docteur",
+    'doctor-one' => "Docteur un",
+    'doctor-two' => "Docteur deux",
+    'doctor-three' => "Docteur trois",
+    'doctor-four' => "Docteur quatre",
+    'doctor-five' => "Docteur cinq",
+    'request-appointment' => "Demander un rendez-vous",
+
+
+    'what-service-we-provide' => "Quels services nous fournissons",
+    'cardiology' => "Cardiologie",
+    'neurology' => "Neurologie",
+    'urology' => "Urologie",
+    'gynecological' => "Gynécologique",
+    'pediatric' => "Pédiatrie",
+    'laboratory' => "Laboratoire",
+
+];
