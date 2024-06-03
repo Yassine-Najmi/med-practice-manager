3e06f2cb4d5bc07cb9acb0d3aa101aadb4aab0b2
Yassine Najmi <yassine.najmi01@gmail.com>
2024-06-03 20:19:31 +0100

    added some statistic numbers to the dashboard index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -36,95 +36,159 @@
             <div class="card overflow-hidden sales-card bg-primary-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">TODAY ORDERS</h6>
+                        <h6 class="mb-3 tx-16 text-white">Les rendez-vous d'aujourd'hui</h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$5,74.12</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-32 font-weight-bold mb-2 text-white ">{{ $todayCount }}</h4>
+                                <p class="mb-0 tx-12 text-white op-7">Nombre total de rendez-vous prévus pour aujourd'hui
+                                </p>
                             </div>
                             <span class="float-right my-auto ml-auto">
-                                <i class="fas fa-arrow-circle-up text-white"></i>
-                                <span class="text-white op-7"> +427</span>
+                                <span class="mdi mdi-calendar-today text-white tx-36"></span>
                             </span>
                         </div>
                     </div>
                 </div>
-                <span id="compositeline" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+                <span id="compositeline" class="pt-1">10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10</span>
+                {{-- <span id="compositeline" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span> --}}
             </div>
         </div>
         <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
             <div class="card overflow-hidden sales-card bg-danger-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">TODAY EARNINGS</h6>
+                        <h6 class="mb-3 tx-16 text-white">Les rendez-vous de demain</h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$1,230.17</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-32 font-weight-bold mb-2 text-white">{{ $tomorrowCount }}</h4>
+                                <p class="mb-0 tx-12 text-white op-7">Nombre total de rendez-vous prévus pour demain</p>
                             </div>
                             <span class="float-right my-auto ml-auto">
-                                <i class="fas fa-arrow-circle-down text-white"></i>
-                                <span class="text-white op-7"> -23.09%</span>
+                                <span class="mdi mdi-view-day text-white tx-36"></span>
+
                             </span>
                         </div>
                     </div>
                 </div>
-                <span id="compositeline2" class="pt-1">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span>
+                <span id="compositeline2" class="pt-1">10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10</span>
+                {{-- <span id="compositeline2" class="pt-1">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span> --}}
             </div>
         </div>
         <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
             <div class="card overflow-hidden sales-card bg-success-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">TOTAL EARNINGS</h6>
+                        <h6 class="mb-3 tx-16 text-white">Les rendez-vous de cette semaine</h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$7,125.70</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-32 font-weight-bold mb-2 text-white">{{ $weekCount }}</h4>
+                                <p class="mb-0 tx-12 text-white op-7">Nombre total de rendez-vous prévus cette semaine</p>
                             </div>
                             <span class="float-right my-auto ml-auto">
-                                <i class="fas fa-arrow-circle-up text-white"></i>
-                                <span class="text-white op-7"> 52.09%</span>
+                                <span class="mdi mdi-view-week text-white tx-36"></span>
                             </span>
                         </div>
                     </div>
                 </div>
-                <span id="compositeline3" class="pt-1">5,10,5,20,22,12,15,18,20,15,8,12,22,5,10,12,22,15,16,10</span>
+                <span id="compositeline3" class="pt-1">10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10</span>
+
+                {{-- <span id="compositeline3" class="pt-1">5,10,5,20,22,12,15,18,20,15,8,12,22,5,10,12,22,15,16,10</span> --}}
             </div>
         </div>
         <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
             <div class="card overflow-hidden sales-card bg-warning-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">PRODUCT SOLD</h6>
+                        <h6 class="mb-3 tx-16 text-white">Les rendez-vous de ce mois</h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">$4,820.50</h4>
-                                <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                <h4 class="tx-32 font-weight-bold mb-2 text-white">{{ $monthCount }}</h4>
+                                <p class="mb-0 tx-12 text-white op-7">Nombre total de rendez-vous prévus ce mois</p>
                             </div>
                             <span class="float-right my-auto ml-auto">
-                                <i class="fas fa-arrow-circle-down text-white"></i>
-                                <span class="text-white op-7"> -152.3</span>
+                                <span class="mdi mdi-calendar-clock text-white tx-36"></span>
                             </span>
                         </div>
                     </div>
                 </div>
-                <span id="compositeline4" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+                <span id="compositeline4" class="pt-1">10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10</span>
+                {{-- <span id="compositeline4" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span> --}}
             </div>
         </div>
     </div>
     <!-- row closed -->
 
-    {{-- <!-- row opened -->
-    <div class="row row-sm">
+    <!-- row opened -->
+    <div class="row row-sm row-deck">
+        <div class="col-md-12 col-lg-4 col-xl-4">
+            <div class="card card-dashboard-eight pb-2">
+                <h6 class="card-title">Les rendez-vous d'aujourd'hui </h6><span
+                    class="d-block mg-b-10 text-muted tx-14">{{ Carbon\Carbon::parse($appointmentsToday[0]->date)->translatedFormat('d F Y') }}</span>
+                <div class="list-group">
+                    @forelse ($appointmentsToday as $item)
+                        <div class="list-group-item border-top-0">
+                            <p class="mdi mdi-account text-primary tx-24"></p>
+                            <p>{{ $item->patient->name }}</p>
+                            <span>{{ Carbon\Carbon::parse($item->time)->translatedFormat('H:i') }}</span>
+                        </div>
+                    @empty
+                        <p class="tx-12 text-muted">Aucun rendez-vous pour aujourd'hui</p>
+                    @endforelse
+                    <div class="list-group-item border-bottom-0 mb-0">
+                        {{-- <p></p><span></span> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12 col-lg-8 col-xl-8">
+            <div class="card card-table-two">
+                <div class="d-flex justify-content-between">
+                    <h4 class="card-title mb-1">Les rendez-vous de cette semaine</h4>
+                    <i class="mdi mdi-dots-horizontal text-gray"></i>
+                </div>
+                <span class="tx-12 tx-muted mb-3 ">{{ Carbon\Carbon::parse($startOfWeek)->translatedFormat('d F Y') }} à
+                    {{ Carbon\Carbon::parse($endOfWeek)->translatedFormat('d F Y') }}</span>
+                <div class="table-responsive country-table">
+                    <table class="table table-striped table-bordered mb-0 text-sm-nowrap text-lg-nowrap text-xl-nowrap">
+                        <thead>
+                            <tr>
+                                <th class="wd-lg-25p">Nom du patient</th>
+                                <th class="wd-lg-25p ">la date</th>
+                                <th class="wd-lg-25p">à partir de</th>
+                                {{-- <th class="wd-lg-25p tx-right">Tax Witheld</th> --}}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($appointmentsWeek as $item)
+                                <tr>
+                                    <td>{{ $item->patient->name }}</td>
+                                    <td class="tx-medium tx-inverse">
+                                        {{ Carbon\Carbon::parse($item->date)->translatedFormat('d F Y') }}</td>
+                                    <td class="tx-medium tx-inverse">
+                                        {{ Carbon\Carbon::parse($item->time)->translatedFormat('H:i') }}</td>
+                                    {{-- <td class="tx-right tx-medium tx-danger">-$45.10</td> --}}
+                                </tr>
+                            @empty
+                                <p class="tx-12 text-muted">Aucun rendez-vous pour cette semaine</p>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /row -->
+
+    <!-- row opened -->
+    {{-- <div class="row row-sm">
         <div class="col-md-12 col-lg-12 col-xl-7">
             <div class="card">
                 <div class="card-header bg-transparent pd-b-0 pd-t-20 bd-b-0">
@@ -164,11 +228,11 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!-- row closed -->
 
     <!-- row opened -->
-    <div class="row row-sm">
+    {{-- <div class="row row-sm">
         <div class="col-xl-4 col-md-12 col-lg-12">
             <div class="card">
                 <div class="card-header pb-1">
@@ -371,98 +435,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!-- row close -->
-
-    <!-- row opened -->
-    <div class="row row-sm row-deck">
-        <div class="col-md-12 col-lg-4 col-xl-4">
-            <div class="card card-dashboard-eight pb-2">
-                <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales
-                    performance revenue based by
-                    country</span>
-                <div class="list-group">
-                    <div class="list-group-item border-top-0">
-                        <i class="flag-icon flag-icon-us flag-icon-squared"></i>
-                        <p>United States</p><span>$1,671.10</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-nl flag-icon-squared"></i>
-                        <p>Netherlands</p><span>$1,064.75</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-gb flag-icon-squared"></i>
-                        <p>United Kingdom</p><span>$1,055.98</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-ca flag-icon-squared"></i>
-                        <p>Canada</p><span>$1,045.49</span>
-                    </div>
-                    <div class="list-group-item">
-                        <i class="flag-icon flag-icon-in flag-icon-squared"></i>
-                        <p>India</p><span>$1,930.12</span>
-                    </div>
-                    <div class="list-group-item border-bottom-0 mb-0">
-                        <i class="flag-icon flag-icon-au flag-icon-squared"></i>
-                        <p>Australia</p><span>$1,042.00</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-12 col-lg-8 col-xl-8">
-            <div class="card card-table-two">
-                <div class="d-flex justify-content-between">
-                    <h4 class="card-title mb-1">Your Most Recent Earnings</h4>
-                    <i class="mdi mdi-dots-horizontal text-gray"></i>
-                </div>
-                <span class="tx-12 tx-muted mb-3 ">This is your most recent earnings for today's
-                    date.</span>
-                <div class="table-responsive country-table">
-                    <table class="table table-striped table-bordered mb-0 text-sm-nowrap text-lg-nowrap text-xl-nowrap">
-                        <thead>
-                            <tr>
-                                <th class="wd-lg-25p">Date</th>
-                                <th class="wd-lg-25p tx-right">Sales Count</th>
-                                <th class="wd-lg-25p tx-right">Earnings</th>
-                                <th class="wd-lg-25p tx-right">Tax Witheld</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>05 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">34</td>
-                                <td class="tx-right tx-medium tx-inverse">$658.20</td>
-                                <td class="tx-right tx-medium tx-danger">-$45.10</td>
-                            </tr>
-                            <tr>
-                                <td>06 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">26</td>
-                                <td class="tx-right tx-medium tx-inverse">$453.25</td>
-                                <td class="tx-right tx-medium tx-danger">-$15.02</td>
-                            </tr>
-                            <tr>
-                                <td>07 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">34</td>
-                                <td class="tx-right tx-medium tx-inverse">$653.12</td>
-                                <td class="tx-right tx-medium tx-danger">-$13.45</td>
-                            </tr>
-                            <tr>
-                                <td>08 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">45</td>
-                                <td class="tx-right tx-medium tx-inverse">$546.47</td>
-                                <td class="tx-right tx-medium tx-danger">-$24.22</td>
-                            </tr>
-                            <tr>
-                                <td>09 Dec 2019</td>
-                                <td class="tx-right tx-medium tx-inverse">31</td>
-                                <td class="tx-right tx-medium tx-inverse">$425.72</td>
-                                <td class="tx-right tx-medium tx-danger">-$25.01</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /row --> --}}
 @endsection
